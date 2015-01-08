2876e277166d035fc7f7a7a19982ffb3539935a7
aysla <aysla001@yahoo.com>
2015-01-07 23:37:16 -0500

    Plots 1,2,3, and 4 JPGs for Plots 1,2,3, and 4

--- plot2.R
+++ plot2.R
@@ -2,17 +2,15 @@
 ## setwd("C:/Users/Angel/Desktop/Stuff/Learning/Coursera/4 Exploratory Data Analysis/Project 1")
 # headers in data: Date Time Global_active_power Global_reactive_power Global_intensity Sub_metering_1 Sub_metering_2 Sub_metering_3
 
-plot1 <- function (){
+plot2 <- function (){
   data <- read.table("household_power_consumption.txt", header = TRUE, sep=";")
   #data1<- strptime(data$Date, format='%d/%m/%Y %H:%M:%S')
   datasub<-subset(data, Date=="1/2/2007"| Date=="2/2/2007")
   #print (head(datasub))
   
-  #graph 1 histrogram. needs to have updated labels to make the x scale kilowatts
-  #dev.copy(png, file = "plot1.png")
-  hist(as.numeric(datasub$Global_active_power), col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
-  #png(filename = "plot1.png")
-  dev.copy(png, file = "plot1.png")
+  #plot2
+  datasub$DateTime <- as.POSIXct(paste(datasub$Date, datasub$Time), format="%d/%m/%Y %H:%M:%S") 
+  plot(datasub$DateTime, as.numeric(datasub$Global_active_power),type="l", ylab="Global Active Power (killowatts)", xlab= "")
+  dev.copy(png, file = "plot2.png")
   dev.off()
-
 }
