0beb16ad0e73c9a1218cb0b45e81620daf07c940
aysla <aysla001@yahoo.com>
2015-01-09 23:26:01 -0500

    Update import specs to na.strings= "?"

--- plot2.R
+++ plot2.R
@@ -3,7 +3,7 @@
 # headers in data: Date Time Global_active_power Global_reactive_power Global_intensity Sub_metering_1 Sub_metering_2 Sub_metering_3
 
 plot2 <- function (){
-  data <- read.table("household_power_consumption.txt", header = TRUE, sep=";")
+  data <- read.table("household_power_consumption.txt", header = TRUE, sep=";", na.strings ="?")
   #data1<- strptime(data$Date, format='%d/%m/%Y %H:%M:%S')
   datasub<-subset(data, Date=="1/2/2007"| Date=="2/2/2007")
   #print (head(datasub))
--- plot4.R
+++ plot4.R
@@ -3,14 +3,14 @@
 # headers in data: Date Time Global_active_power Global_reactive_power Global_intensity Sub_metering_1 Sub_metering_2 Sub_metering_3
 
 plot4 <- function (){
-  data <- read.table("household_power_consumption.txt", header = TRUE, sep=";")
+  data <- read.table("household_power_consumption.txt", header = TRUE, sep=";", na.strings ="?")
   #data1<- strptime(data$Date, format='%d/%m/%Y %H:%M:%S')
   datasub<-subset(data, Date=="1/2/2007"| Date=="2/2/2007")
   #print (head(datasub))
   
   #graph4 multiple plots
   datasub$DateTime <- as.POSIXct(paste(datasub$Date, datasub$Time), format="%d/%m/%Y %H:%M:%S") 
-  par(mfrow = c(2, 2), mar = c(5, 4, 2, 1))
+  par(mfrow = c(2, 2), mar=c(4.5,3,0,0))
   with (datasub, {
     plot(datasub$DateTime, as.numeric(datasub$Global_active_power),type="l", ylab="Global Active Power", xlab= "")
     plot(datasub$DateTime, as.numeric(datasub$Voltage),type="l", ylab="Voltage", xlab= "datetime")
@@ -20,6 +20,6 @@ plot4 <- function (){
     legend("topright", pch =1, col = c("black","red","blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
     plot(datasub$DateTime, as.numeric(datasub$Global_reactive_power),type="l", ylab="Global_reactive_power", xlab= "datetime")
   })
-  dev.copy(png, file = "plot4.png")
+  dev.copy(png, file = "plot4.png",width = 480, height = 480)
   dev.off()
 }
